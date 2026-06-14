b6d3146c88ee6e3d52dfde157346e3dacab2557a
Rahil <rahilsardar234@gmail.com>
2026-06-15 01:12:15 +0530

    Add Open Graph meta tags for improved WhatsApp link previews and update product IDs in script.js

--- src/script.js
+++ src/script.js
@@ -1,7 +1,7 @@
 const products = [
     // Local Products
     {
-        id: 1,
+        id: 2,
         title: "Abiraterone Acetate (Ahabir) 500 mg",
         category: "Anti Cancer Medicines",
         price: "Price on Request",
@@ -9,79 +9,80 @@ const products = [
         inStock: true
     },
     {
-        id: 2,
+        id: 3,
         title: "Pazopanib (PAZINIB) 200 mg",
         category: "Anti Cancer Medicines",
+        price: "Price on Request",
         image: "../assets/products/PHOTO-2026-06-12-20-10-42.jpg",
-        price: "Price on Request",
         inStock: true
-    },
-    {
-        id: 3,
-        title: "Episiotomy Scissors",
-        category: "Surgical Instruments",
-        image: "../assets/products/PHOTO-2026-06-12-20-11-20.jpg",
-        price: "Price on Request",
-        inStock: false
     },
     {
         id: 4,
-        title: "Crocodile Forceps",
+        title: "Episiotomy Scissors",
         category: "Surgical Instruments",
-        image: "../assets/products/PHOTO-2026-06-12-20-11-30.jpg",
         price: "Price on Request",
-        inStock: true
+        image: "../assets/products/PHOTO-2026-06-12-20-11-20.jpg",
+        inStock: false
     },
     {
         id: 5,
+        title: "Crocodile Forceps",
+        category: "Surgical Instruments",
+        price: "Price on Request",
+        image: "../assets/products/PHOTO-2026-06-12-20-11-30.jpg",
+        inStock: true
+    },
+    {
+        id: 6,
         title: "Mycophenolate Mofetil (CellCept) 500 mg (Box)",
         category: "Pharmaceutical Tablets",
-        image: "../assets/products/PHOTO-2026-06-12-20-14-21.jpg",
         price: "Price on Request",
-        inStock: false
+        image: "../assets/products/PHOTO-2026-06-12-20-14-21.jpg",
+        inStock: true
     },
     // Products fetched from IndiaMART Profile
-    {
-        id: 7,
-        title: "Mycophenolic Acid Tablet 360 mg",
-        category: "Pharmaceutical Tablets",
-        price: "Price on Request",
-        image: "https://5.imimg.com/data5/SELLER/Default/2023/7/325120222/RX/WL/OR/11404443/mycophenolic-acid-tablet-360-mg-250x250.jpg",
-        inStock: true
-    },
-    {
-        id: 8,
-        title: "Tenofovir Disoproxil Fumarate 300mg Tablets samhep-TDF",
-        category: "Pharmaceutical Tablets",
-        price: "Price on Request",
-        image: "https://5.imimg.com/data5/ANDROID/Default/2023/8/338988962/XP/ZT/DT/11404443/product-jpeg-250x250.jpeg",
-        inStock: true
-    },
-    {
-        id: 9,
-        title: "Taxeleon Injection 260MG",
-        category: "Anti Cancer Medicines",
-        price: "Price on Request",
-        image: "https://5.imimg.com/data5/IOS/Default/2024/10/457995027/SZ/PB/LJ/11404443/product-jpeg-250x250.png",
-        inStock: true
-    },
-    {
-        id: 10,
-        title: "Dr. Odin Pulse Oximeter",
-        category: "Medical Devices",
-        price: "Price on Request",
-        image: "https://5.imimg.com/data5/IOS/Default/2024/10/457996720/HZ/QG/AK/11404443/product-jpeg-250x250.png",
-        inStock: true
-    },
-    {
-        id: 11,
-        title: "Perfecxa Digital Thermometer",
-        category: "Medical Devices",
-        price: "Price on Request",
-        image: "https://5.imimg.com/data5/SELLER/Default/2025/6/520115034/HN/IF/UE/11404443/perfecxa-digital-thermometer-250x250.jpeg",
-        inStock: true
-    }
+    { id: 7, title: "Mycophenolic Acid Tablet 360 mg", category: "Pharmaceutical Tablets", price: "Price on Request", image: "https://5.imimg.com/data5/SELLER/Default/2023/7/325120222/RX/WL/OR/11404443/mycophenolic-acid-tablet-360-mg-250x250.jpg", inStock: true },
+    { id: 8, title: "Tenofovir Disoproxil Fumarate 300mg Tablets samhep-TDF", category: "Pharmaceutical Tablets", price: "Price on Request", image: "https://5.imimg.com/data5/ANDROID/Default/2023/8/338988962/XP/ZT/DT/11404443/product-jpeg-250x250.jpeg", inStock: true },
+    { id: 9, title: "Taxeleon Injection 260MG", category: "Anti Cancer Medicines", price: "Price on Request", image: "https://5.imimg.com/data5/IOS/Default/2024/10/457995027/SZ/PB/LJ/11404443/product-jpeg-250x250.png", inStock: true },
+    { id: 10, title: "Dr. Odin Pulse Oximeter", category: "Medical Devices", price: "Price on Request", image: "https://5.imimg.com/data5/IOS/Default/2024/10/457996720/HZ/QG/AK/11404443/product-jpeg-250x250.png", inStock: true },
+    { id: 11, title: "Perfecxa Digital Thermometer", category: "Medical Devices", price: "Price on Request", image: "https://5.imimg.com/data5/SELLER/Default/2025/6/520115034/HN/IF/UE/11404443/perfecxa-digital-thermometer-250x250.jpeg", inStock: true },
+    { id: 12, title: "Oluminant Baricitinib 4mg", category: "Pharmaceutical Tablets", price: "Price on Request", image: "https://5.imimg.com/data5/ANDROID/Default/2024/8/444972783/LP/LH/VB/11404443/product-jpeg-250x250.jpg", inStock: true },
+    { id: 13, title: "Amiodarone Injection tachydon 150 mg", category: "Pharmaceutical Injections", price: "Price on Request", image: "https://5.imimg.com/data5/ANDROID/Default/2023/8/338732845/OB/YP/MG/11404443/product-jpeg-250x250.jpeg", inStock: true },
+    { id: 14, title: "Gadopentetate Dimeglumine Injection Usp Gadoimage", category: "Pharmaceutical Injections", price: "Price on Request", image: "https://5.imimg.com/data5/ANDROID/Default/2023/8/338966188/GB/BQ/IP/11404443/product-jpeg-250x250.jpeg", inStock: true },
+    { id: 15, title: "Protamine Sulphate Injection prota 50 mg", category: "Pharmaceutical Injections", price: "Price on Request", image: "https://5.imimg.com/data5/ANDROID/Default/2023/8/338970664/PH/MC/LY/11404443/product-jpeg-250x250.jpeg", inStock: true },
+    { id: 16, title: "Polymyxin B For Injection Usp POLYHET", category: "Pharmaceutical Injections", price: "Price on Request", image: "https://5.imimg.com/data5/ANDROID/Default/2023/8/339119391/YQ/YF/DI/11404443/product-jpeg-250x250.jpeg", inStock: true },
+    { id: 17, title: "Enoxaparin Sodium Injection", category: "Pharmaceutical Injections", price: "Price on Request", image: "https://5.imimg.com/data5/ANDROID/Default/2024/8/444974453/SY/SV/HH/11404443/product-jpeg-250x250.jpg", inStock: true },
+    { id: 18, title: "Vorier Voriconazole 200mg", category: "Pharmaceutical Tablets", price: "Price on Request", image: "https://5.imimg.com/data5/ANDROID/Default/2023/8/338754222/JM/PR/DK/11404443/product-jpeg-250x250.jpeg", inStock: true },
+    { id: 19, title: "Leuprolide Acetate LUPRODEX 11.25", category: "Anti Cancer Medicines", price: "Price on Request", image: "https://5.imimg.com/data5/ANDROID/Default/2023/8/338778732/LZ/DW/DE/11404443/product-jpeg-250x250.jpeg", inStock: true },
+    { id: 20, title: "Cytodrox Hydroxyurea Capsule", category: "Anti Cancer Medicines", price: "Price on Request", image: "https://5.imimg.com/data5/ANDROID/Default/2023/8/338804029/BO/AD/GW/11404443/product-jpeg-250x250.jpeg", inStock: true },
+    { id: 21, title: "Capegard 500mg Capecitabine Tablet", category: "Anti Cancer Medicines", price: "Price on Request", image: "https://5.imimg.com/data5/ANDROID/Default/2023/8/338805315/FV/HU/CX/11404443/product-jpeg-250x250.jpeg", inStock: true },
+    { id: 22, title: "Tegafur Gimeracil And Oteracil P Capsule", category: "Anti Cancer Medicines", price: "Price on Request", image: "https://5.imimg.com/data5/ANDROID/Default/2024/8/444974256/HU/UB/MP/11404443/product-jpeg-250x250.jpg", inStock: true },
+    { id: 23, title: "Velpanat Sofosbuvir 400 Mg Velpatasvir 100Mg", category: "Pharmaceutical Tablets", price: "Price on Request", image: "https://5.imimg.com/data5/IOS/Default/2024/10/457058459/TS/VH/YW/11404443/product-jpeg-250x250.png", inStock: true },
+    { id: 24, title: "Atormit 40mg Atorvastatin", category: "Pharmaceutical Tablets", price: "Price on Request", image: "https://5.imimg.com/data5/SELLER/PDFImage/2025/1/477818796/YR/CU/AU/11404443/atormit-40mg-atorvastatin-250x250.png", inStock: true },
+    { id: 25, title: "Duovir N Tab", category: "Pharmaceutical Tablets", price: "Price on Request", image: "https://5.imimg.com/data5/ANDROID/Default/2023/8/338799393/ZA/YB/VL/11404443/product-jpeg-250x250.jpeg", inStock: true },
+    { id: 26, title: "Entavir Entecavir 1mg Tablet", category: "Pharmaceutical Tablets", price: "Price on Request", image: "https://5.imimg.com/data5/ANDROID/Default/2023/8/338809299/SE/EF/TU/11404443/product-jpeg-250x250.jpeg", inStock: true },
+    { id: 27, title: "Takfa 1 Tacrolimus Capsule", category: "Pharmaceutical Capsules", price: "Price on Request", image: "https://5.imimg.com/data5/SELLER/Default/2023/7/326871405/II/UI/EV/11404443/takfa-1-tacrolimus-capsule-250x250.jpg", inStock: true },
+    { id: 28, title: "Tacrolimus Capsule IP 1 mg", category: "Pharmaceutical Capsules", price: "Price on Request", image: "https://5.imimg.com/data5/SELLER/Default/2023/7/326872867/PD/ZX/PX/11404443/tacrolimus-capsule-ip-1-mg-250x250.jpeg", inStock: true },
+    { id: 29, title: "Cyclosporine Capsules IP 500 mg", category: "Pharmaceutical Capsules", price: "Price on Request", image: "https://5.imimg.com/data5/SELLER/Default/2023/7/326874600/TY/FK/NV/11404443/cyclosporine-capsules-ip-500-mg-250x250.jpg", inStock: true },
+    { id: 30, title: "Tacrolimus Capsule IP 0.5 mg", category: "Pharmaceutical Capsules", price: "Price on Request", image: "https://5.imimg.com/data5/SELLER/Default/2023/7/326873879/UC/MZ/QA/11404443/tacrolimus-capsule-ip-0-5-mg-250x250.jpeg", inStock: true },
+    { id: 31, title: "Leuprolide Acetate LUPRODEX 22.5", category: "Anti Cancer Medicines", price: "Price on Request", image: "https://5.imimg.com/data5/ANDROID/Default/2023/8/338780987/MK/DF/BT/11404443/product-jpeg-250x250.jpeg", inStock: true },
+    { id: 32, title: "Pomide 2mg Capsules", category: "Anti Cancer Medicines", price: "Price on Request", image: "https://5.imimg.com/data5/ANDROID/Default/2023/8/339173946/YQ/ZQ/IT/11404443/product-jpeg-250x250.jpeg", inStock: true },
+    { id: 33, title: "Posacad Gr POSACAD GR", category: "Pharmaceutical Tablets", price: "Price on Request", image: "https://5.imimg.com/data5/ANDROID/Default/2023/8/338737939/UA/VV/QA/11404443/product-jpeg-250x250.jpeg", inStock: true },
+    { id: 34, title: "Imatib 400 Mg Tablets", category: "Anti Cancer Medicines", price: "Price on Request", image: "https://5.imimg.com/data5/ANDROID/Default/2023/8/338825081/XD/WE/FB/11404443/product-jpeg-250x250.jpeg", inStock: true },
+    { id: 35, title: "Reviro Tenofovir Tablet 300MG", category: "Pharmaceutical Tablets", price: "Price on Request", image: "https://5.imimg.com/data5/ANDROID/Default/2023/8/338958026/WG/PX/JO/11404443/product-jpeg-250x250.jpeg", inStock: true },
+    { id: 36, title: "Cefoperazone Sulbactam Injection kelxone-s", category: "Pharmaceutical Injections", price: "Price on Request", image: "https://5.imimg.com/data5/ANDROID/Default/2023/8/338734621/UV/ZV/LF/11404443/product-jpeg-250x250.jpeg", inStock: true },
+    { id: 37, title: "Non Contact Thermometer", category: "Medical Devices", price: "Price on Request", image: "https://5.imimg.com/data5/IOS/Default/2024/10/457996931/UJ/XM/FK/11404443/product-jpeg-250x250.png", inStock: true }
 ];
+
+// Auto-generate realistic stock counts for the prototype
+products.forEach(p => {
+    if (p.inStock && p.stockCount === undefined) {
+        // Generates a consistent number between 15 and 95 based on the product ID
+        p.stockCount = (p.id * 17) % 80 + 15; 
+    } else if (!p.inStock) {
+        p.stockCount = 0;
+    }
+});
 
 const phoneNumber = "919846196746";
 
@@ -96,17 +97,17 @@ function init() {
 
 function renderCategories() {
     const categoryList = document.getElementById('category-list');
-
+    
     // Extract unique categories
     const categories = ["All Categories", ...new Set(products.map(p => p.category))];
-
+    
     categoryList.innerHTML = categories.map(cat => `
         <li class="${cat === currentCategory ? 'active' : ''}" onclick="setCategory('${cat}')">${cat}</li>
     `).join('');
 }
 
 // Make setCategory global so inline onclick can access it
-window.setCategory = function (cat) {
+window.setCategory = function(cat) {
     currentCategory = cat;
     renderCategories();
     renderProducts();
@@ -126,7 +127,7 @@ function setupSearch() {
     searchInput.addEventListener('keyup', (e) => {
         if (e.key === 'Enter') handleSearch();
     });
-
+    
     // Live search
     searchInput.addEventListener('input', handleSearch);
 }
@@ -134,17 +135,17 @@ function setupSearch() {
 function renderProducts() {
     const grid = document.getElementById('product-grid');
     const countEl = document.getElementById('product-count');
-
+    
     let filtered = products;
-
+    
     if (currentCategory !== "All Categories") {
         filtered = filtered.filter(p => p.category === currentCategory);
     }
-
+    
     if (searchQuery.trim() !== "") {
         filtered = filtered.filter(p => p.title.toLowerCase().includes(searchQuery));
     }
-
+    
     countEl.innerText = `${filtered.length} items found`;
     grid.innerHTML = '';
 
@@ -169,6 +170,10 @@ function renderProducts() {
         const btnText = product.inStock ? 'Contact Supplier' : 'Unavailable';
         const btnDisabled = !product.inStock ? 'disabled' : '';
 
+        const stockText = product.inStock 
+            ? `<p style="color: #059669; font-size: 0.875rem; font-weight: 500; margin-bottom: 0.75rem;">✓ ${product.stockCount} In Stock</p>`
+            : '<p style="color: #dc2626; font-size: 0.875rem; font-weight: 500; margin-bottom: 0.75rem;">✗ 0 In Stock</p>';
+
         card.innerHTML = `
             <div class="product-image-container">
                 ${badgeHtml}
@@ -177,17 +182,18 @@ function renderProducts() {
             <h3 class="product-title">${product.title}</h3>
             <p class="product-price">${product.price}</p>
             <p class="product-category">${product.category}</p>
+            ${stockText}
             <button class="${btnClass}" ${btnDisabled} onclick="sendToWhatsApp(${product.id})">
                 ${whatsappIcon}
                 ${btnText}
             </button>
         `;
-
+        
         grid.appendChild(card);
     });
 }
 
-window.sendToWhatsApp = function (productId) {
+window.sendToWhatsApp = function(productId) {
     const product = products.find(p => p.id === productId);
     if (!product || !product.inStock) return;
 
@@ -204,7 +210,7 @@ window.sendToWhatsApp = function (productId) {
 Hello Yuzaxy team, I am interested in this product and would like more details.`;
     const encodedMessage = encodeURIComponent(message);
     const whatsappUrl = `https://wa.me/${phoneNumber}?text=${encodedMessage}`;
-
+    
     window.open(whatsappUrl, '_blank');
 }
 
